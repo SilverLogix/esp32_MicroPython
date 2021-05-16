89207aab5d0e930a90d3171b721177bfd3b73c32
SilverLogix <cameron.c.ait@gmail.com>
2021-05-16 17:11:06 -0500

    New way to display screen numbers!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import statvfs
+import framebuf
 import machine
 from machine import Pin, I2C, ADC, PWM
 import dht
@@ -7,7 +8,6 @@ import ssd1306
 import esp32
 # Custom imports
 from boot import gc, ap
-from logo import showlogo
 
 # Init Screen --------------------------------------------------#
 i2c = I2C(-1, scl=Pin(22), sda=Pin(21))  # Set Pins
@@ -52,33 +52,43 @@ led = Pin(2, Pin.OUT)
 adc = ADC(Pin(35))
 stop = bool(False)
 
+def oled_pix(loc):
+    opix = [125, 120, 115, 110, 105, 100]
+    for x in opix:
+        oled.pixel(x, 4, 1)
 
-def oled_pix():
-    oled.pixel(70, 4, 1)
-    oled.pixel(75, 4, 1)
-    oled.pixel(80, 4, 1)
-    oled.pixel(85, 4, 1)
+    if loc == 0:
+        ll = 100
+    if loc == 1:
+        ll = 105
+    if loc == 2:
+        ll = 110
+    if loc == 3:
+        ll = 115
+    if loc == 4:
+        ll = 120
+    if loc == 5:
+        ll = 125
+    if loc == 6:
+        ll = 130
 
-    oled.pixel(90, 3, 1)
-    oled.pixel(89, 4, 1)
-    oled.pixel(90, 5, 1)
-    oled.pixel(91, 4, 1)
+    oled.pixel(ll-1, 4, 1)
+    oled.pixel(ll+1, 4, 1)
+    oled.pixel(ll, 4-1, 1)
+    oled.pixel(ll, 4+1, 1)
+
 
 
 # (Title, Line#1, Line#2, Line#3, Line#4, Line#5, Screen#)
-def oled_text(otitle, oline1, oline2, oline3, oline4, oline5, onum):
-    oled.fill(0)
-
+def oled_text(otitle, oline1, oline2, oline3, oline4, oline5):
     oled.text(otitle, 0, 0)
-    oled.text(onum, 90, 0)
+    # oled.text(onum, 90, 0)
 
     oled.text(oline1, 0, 16)
     oled.text(oline2, 0, 26)
     oled.text(oline3, 0, 36)
     oled.text(oline4, 0, 46)
     oled.text(oline5, 0, 56)
-
-    oled.show()
 
 
 def deep_sleep():
@@ -106,22 +116,28 @@ def temp():  # Get and display temperature # SCREEN 1
         fh = (t * 9 / 5) + 32 - 4  # -4 was calibration for my sensor
 
         # Print to screen
-        oled_text("Sensor", "Temp = {0:0.1f}F".format(fh), "Humidity = {0:0.0f}%".format(h), "", "", "", ".|...")
+        oled.fill(0)
+        oled_pix(1)
+        oled_text("Sensor", "Temp = {0:0.1f}F".format(fh), "Humidity = {0:0.0f}%".format(h), "", "", "")
+        oled.show()
 
     except OSError as e:
-        oled_text("Error", "Failed to", "read sensor", "", "", "", "2")
+        oled.fill(0)
+        oled_text("Error", "Failed to", "read sensor", "", "", "")
+        oled.show()
         return "Failed to read sensor."
 
 
 def fail():  # Test fail screen # SCREEN 4
     oled.fill(0)
+    oled_pix(4)
     oled.text("Draw", 0, 0)  # Set some text
-    #oled.fill_rect(15, 15, 44, 44, 1)
+    # oled.fill_rect(15, 15, 44, 44, 1)
     # oled.rect(10, 10, 40, 40, 1)
     # oled.line(0,0,64,64,1)
     # oled.triangle(10, 10, 55, 20, 5, 40, 1)
     # oled.circle(64, 32, 10, 1)
-    oled.round_rect(20,20,30,30,3,1)
+    oled.round_rect(20, 20, 30, 30, 3, 1)
     oled.show()
 
 
@@ -135,7 +151,10 @@ def info():  # Get and display Info # SCREEN 2
     raw = str(esp32.raw_temperature())
     print(ip)
 
-    oled_text("Info", ip, "CPU temp = " + raw + "F", "", mfreq, "", "..|..")
+    oled.fill(0)
+    oled_pix(2)
+    oled_text("Info", ip, "CPU temp = " + raw + "F", "", mfreq, "", )
+    oled.show()
     gc.collect()
 
 
@@ -152,15 +171,33 @@ def dfree():  # Display remaining free space # SCREEN 3
     mbfree = freesize / mbcalc  # 0.046875
     freestr = str(mbfree)
 
-    oled_text("Space(MB)", "curr: " + freestr, "", "Old:  " + "1.949219", "", am, "...|.")
+    oled.fill(0)
+    oled_pix(3)
+    oled_text("Space(MB)", "curr: " + freestr, "", "Old:  " + "1.949219", "", am)
+    oled.show()
     gc.collect()
 
 
-def scralarm():
+def scralarm():  # SCREEN #0
     var = str(adc.read())
     am = str(alarm_1)
 
-    oled_text("Screen", "Alarm_1 = " + am, var, "", "", "", "|....")
+    oled.fill(0)
+    oled_pix(0)
+    oled_text("Screen", "Alarm_1 = " + am, var, "", "", "")
+    oled.show()
+
+
+def showlogo():
+    fb_smile1 = framebuf.FrameBuffer(bytearray(b'\x00~\x00\x03\xff\xc0\x07\x81\xe0\x1e\x00x8\x00\x1c0\x00\x0c`\x00\x0ea\xc3\x86\xe0\x00\x07\xc0\x00\x03\xc0\x00\x03\xc0\x00\x02\xc0\x00\x03\xc0\x00\x03\xe0B\x07`<\x06`\x00\x060\x00\x0c8\x00\x1c\x1e\x00x\x07\x81\xe0\x03\xff\xc0\x00\xff\x00'), 24, 23, framebuf.MONO_HLSB)
+
+    oled.fill(0)
+    oled.framebuf.blit(fb_smile1, 0, 20)
+    oled.text("Logo", 0, 0)
+    oled.text("Hello", 30, 23)
+    oled.text("MicroPython", 30, 33)
+    oled_pix(5)
+    oled.show()
 
 
 # On Interupt button ------------------------------------------------------------------- #
